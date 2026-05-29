e68fb5544acfffd8e48a782f96237a4afa31d0ac
1stPlayer <phoenix_vio@live.com>
2026-05-29 19:47:26 +0800

    Bump to v1.1.6

--- Casks/mpv-macwrapper.rb
+++ Casks/mpv-macwrapper.rb
@@ -1,6 +1,6 @@
 cask "mpv-macwrapper" do
-  version "1.1.5"
-  sha256 "192b6bada7d8c6f48ede8a4307c36cbb87f9f8333c73f2cbde992889d727cc20"
+  version "1.1.6"
+  sha256 "75b593ae55540eb38ea920242740500b2053bb36a2c4d307b05a4e2397e3ab21"
 
   url "https://github.com/IstPlayer/mpv-macWrapper/releases/download/v#{version}/mpv.dmg"
   name "mpv-macWrapper"
